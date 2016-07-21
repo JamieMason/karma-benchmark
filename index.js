587b6b5bb84e7c83a58f4dc860772e8dd8dd90f2
Jamie Mason <siunmaru@gmail.com>
2016-07-21 22:48:54 +0100

    refactor(npm): use benchmark@1.0.0 from npm

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-var createPattern = function(path) {
-  return { pattern: path, included: true, served: true, watched: false };
-};
-
-var initBenchmark = function(files) {
-  files.unshift(createPattern(__dirname + '/adapter.js'));
-  files.unshift(createPattern(__dirname + '/lib/benchmark.js'));
-  files.unshift(createPattern(__dirname + '/lib/perftacular.js'));
-};
-
 initBenchmark.$inject = ['config.files'];
 
 module.exports = {
   'framework:benchmark': ['factory', initBenchmark]
 };
+
+function initBenchmark(files) {
+  ['./src/perftacular.js', './src/adapter.js', 'benchmark']
+    .forEach(registerFile);
+
+  function registerFile(selector) {
+    files.unshift({
+      included: true,
+      pattern: require.resolve(selector),
+      served: true,
+      watched: false
+    });
+  }
+}
