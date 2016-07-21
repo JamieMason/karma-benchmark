85de0e5a2702e31c730f0129e389bd8361730f14
Jamie Mason <siunmaru@gmail.com>
2016-07-22 00:59:55 +0100

    docs(examples): add example using karma-benchmark and reporter

---
+++ examples/bench/array.bench.js
@@ -0,0 +1,13 @@
+suite('Array iteration', function() {
+  benchmark('_.each', function() {
+    _.each([1, 2, 3], function(el) {
+      return el;
+    });
+  });
+
+  benchmark('native forEach', function() {
+    [1, 2, 3].forEach(function(el) {
+      return el;
+    });
+  });
+});
---
+++ examples/karma.conf.js
@@ -0,0 +1,31 @@
+
+module.exports = function(config) {
+  config.set({
+    autoWatch: false,
+    basePath: '',
+    browsers: [
+      'PhantomJS'
+    ],
+    colors: true,
+    concurrency: Infinity,
+    exclude: [],
+    files: [
+      'bench/**/*.bench.js'
+    ],
+    frameworks: [
+      'benchmark'
+    ],
+    junitReporter: {
+      outputDir: 'reports',
+      outputFile: 'benchmark.xml'
+    },
+    logLevel: config.LOG_INFO,
+    port: 9876,
+    preprocessors: {},
+    reporters: [
+      'benchmark',
+      'junit'
+    ],
+    singleRun: true
+  });
+};
