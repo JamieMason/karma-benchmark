efd26f9689a1e2064a72e084633af3a4209272cd
Jamie Mason <jamie@foldleft.io>
2016-09-30 09:14:22 +0100

    docs(examples): add suite to example

--- examples/bench/array.bench.js
+++ examples/bench/array.bench.js
@@ -1,14 +1,32 @@
 /* global suite, benchmark, _ */
-suite('Array iteration', function () {
-  benchmark('_.each', function () {
-    _.each([1, 2, 3], function (el) {
+suite('Array iteration', function() {
+  benchmark('_.each', function() {
+    _.each([1, 2, 3], function(el) {
       return el;
     });
   });
 
-  benchmark('native forEach', function () {
-    [1, 2, 3].forEach(function (el) {
+  benchmark('native forEach', function() {
+    [1, 2, 3].forEach(function(el) {
       return el;
+    });
+  });
+});
+
+suite('Plucking key names', function() {
+  benchmark('_.keys', function() {
+    _.keys({
+      a: 1,
+      b: 2,
+      c: 3
+    });
+  });
+
+  benchmark('Object.keys', function() {
+    Object.keys({
+      a: 1,
+      b: 2,
+      c: 3
     });
   });
 });
